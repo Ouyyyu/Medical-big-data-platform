e3c4ee0446cc186c9dbdeb77842372f708d6e15b
Ouyyyu <103258786+Ouyyyu@users.noreply.github.com>
2023-05-21 16:26:10 +0800

    Delete backend directory

--- backend/backend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("user/", include("user.urls")),
-    path("baike/", include("baike.urls")),
-    path("robot", include("robot.urls")),
-]
